b4e915eb9c8e6487ac79fb0e95816da7a52531cf
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-10-04 23:03:08 +0900

    디자인시스템 모듈에서 아이콘 관리하도록 함.

---
+++ core/designsystem/src/main/java/com/bossmg/android/designsystem/ui/icons/LifeIcons.kt
@@ -0,0 +1,18 @@
+package com.bossmg.android.designsystem.ui.icons
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.rounded.CameraAlt
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.Share
+
+object LifeIcons {
+    val ArrowLeft = Icons.AutoMirrored.Filled.KeyboardArrowLeft
+    val ArrowRight = Icons.AutoMirrored.Filled.KeyboardArrowRight
+    val Photo = Icons.Rounded.CameraAlt
+    val Save = Icons.Rounded.Check
+    val Delete = Icons.Rounded.Delete
+    val Share = Icons.Rounded.Share
+}
