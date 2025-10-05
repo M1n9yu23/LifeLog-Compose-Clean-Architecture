199f1e2924fc452e1bc9d165ef5f2bed7c86bb59
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-10-05 17:05:10 +0900

    리포지토리 구현

--- core/data/src/main/java/com/bossmg/android/data/repository/LifeLogRepositoryImpl.kt
+++ core/data/src/main/java/com/bossmg/android/data/repository/LifeLogRepositoryImpl.kt
@@ -1,35 +1,44 @@
 package com.bossmg.android.data.repository
 
+import com.bossmg.android.data.database.LifeLogDao
+import com.bossmg.android.data.mapper.LifeLogMapper
 import com.bossmg.android.domain.model.LifeLog
 import com.bossmg.android.domain.repository.LifeLogRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
-class LifeLogRepositoryImpl : LifeLogRepository {
-    override fun getLifeLogs(): Flow<List<LifeLog>> {
-        TODO("Not yet implemented")
-    }
+class LifeLogRepositoryImpl @Inject constructor(
+    private val dao: LifeLogDao,
+    private val mapper: LifeLogMapper
+) : LifeLogRepository {
+    override fun getLifeLogs(): Flow<List<LifeLog>> =
+        dao.getLifeLogs().map { entities ->
+            entities.map { mapper.map(it) }
+        }
 
-    override fun getLifeLogsByDate(date: String): Flow<List<LifeLog>> {
-        TODO("Not yet implemented")
-    }
+    override fun getLifeLogsByDate(date: String): Flow<List<LifeLog>> =
+        dao.getLifeLogsByDate(date).map { entities ->
+            entities.map { mapper.map(it) }
+        }
 
-    override fun getLifeLogsByMood(mood: String): Flow<List<LifeLog>> {
-        TODO("Not yet implemented")
-    }
+    override fun getLifeLogsByMood(mood: String): Flow<List<LifeLog>> =
+        dao.getLifeLogsByMood(mood).map { entities ->
+            entities.map { mapper.map(it) }
+        }
 
-    override suspend fun getLifeLogById(id: Int): LifeLog {
-        TODO("Not yet implemented")
+    override suspend fun getLifeLogById(id: Int): LifeLog = dao.getLifeLogById(id).run {
+        mapper.map(this)
     }
 
     override suspend fun insertLifeLog(lifeLog: LifeLog) {
-        TODO("Not yet implemented")
+        dao.insertLifeLog(mapper.mapBack(lifeLog))
     }
-
     override suspend fun upsertLifeLog(lifeLog: LifeLog) {
-        TODO("Not yet implemented")
+        dao.upsertLifeLog(mapper.mapBack(lifeLog))
     }
 
     override suspend fun deleteLifeLogById(id: Int) {
-        TODO("Not yet implemented")
+        dao.deleteLifeLogById(id)
     }
 }
