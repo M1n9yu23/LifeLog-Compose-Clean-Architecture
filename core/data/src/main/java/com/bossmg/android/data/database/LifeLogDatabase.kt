36620281b94c03b53833d7db5c91013354b25f8b
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-10-05 17:04:27 +0900

    엔티티 수정

--- core/data/src/main/java/com/bossmg/android/data/database/LifeLogDatabase.kt
+++ core/data/src/main/java/com/bossmg/android/data/database/LifeLogDatabase.kt
@@ -2,9 +2,9 @@ package com.bossmg.android.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.bossmg.android.data.model.LifeLog
+import com.bossmg.android.data.model.LifeLogEntity
 
-@Database(entities = [LifeLog::class], version = 1)
+@Database(entities = [LifeLogEntity::class], version = 1)
 internal abstract class LifeLogDatabase : RoomDatabase() {
     abstract fun lifeLogDao(): LifeLogDao
 }
