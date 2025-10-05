44c1cd7f2f0dd8b8b2026bb84c6293a549a85b37
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-10-05 21:32:25 +0900

    기본값을 명시해줌

--- core/domain/src/main/java/com/bossmg/android/domain/model/LifeLog.kt
+++ core/domain/src/main/java/com/bossmg/android/domain/model/LifeLog.kt
@@ -1,10 +1,13 @@
 package com.bossmg.android.domain.model
 
+import com.bossmg.android.domain.util.MoodProvider
+import java.time.LocalDate
+
 data class LifeLog(
-    val id: Int,
-    val date: String,
-    val title: String,
-    val description: String,
-    val mood: String,
-    val img: String?
+    val id: Int = 0,
+    val date: String = LocalDate.now().toString(),
+    val title: String = "",
+    val description: String = "",
+    val mood: String = MoodProvider.Moods.first().str,
+    val img: String? = null
 )
