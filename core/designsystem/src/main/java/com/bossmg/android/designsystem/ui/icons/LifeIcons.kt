2045e0d45e655e7abf5a02f218cc03356fd5d0f1
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-10-05 14:46:05 +0900

    아이콘 추가

--- core/designsystem/src/main/java/com/bossmg/android/designsystem/ui/icons/LifeIcons.kt
+++ core/designsystem/src/main/java/com/bossmg/android/designsystem/ui/icons/LifeIcons.kt
@@ -3,9 +3,14 @@ package com.bossmg.android.designsystem.ui.icons
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.automirrored.rounded.FormatListBulleted
+import androidx.compose.material.icons.outlined.Photo
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.CalendarMonth
 import androidx.compose.material.icons.rounded.CameraAlt
 import androidx.compose.material.icons.rounded.Check
 import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.Mood
 import androidx.compose.material.icons.rounded.Share
 
 object LifeIcons {
@@ -15,4 +20,9 @@ object LifeIcons {
     val Save = Icons.Rounded.Check
     val Delete = Icons.Rounded.Delete
     val Share = Icons.Rounded.Share
+    val Home = Icons.AutoMirrored.Rounded.FormatListBulleted
+    val Calendar = Icons.Rounded.CalendarMonth
+    val Mood = Icons.Rounded.Mood
+    val PhotoTab = Icons.Outlined.Photo
+    val Add = Icons.Rounded.Add
 }
