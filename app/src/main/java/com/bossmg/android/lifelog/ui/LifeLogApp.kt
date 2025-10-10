51f7939ae3b6a37c24a7e2b2806950dac61bf681
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-10-10 14:00:46 +0900

    바텀바 만큼 패딩을 가지도록 수정

--- app/src/main/java/com/bossmg/android/lifelog/ui/LifeLogApp.kt
+++ app/src/main/java/com/bossmg/android/lifelog/ui/LifeLogApp.kt
@@ -1,11 +1,13 @@
 package com.bossmg.android.lifelog.ui
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavDestination.Companion.hasRoute
 import com.bossmg.android.designsystem.ui.components.LifeLogScaffold
@@ -65,6 +67,6 @@ fun LifeLogApp(
             }
         }
     ) {
-        LifeLogNavHost(appState)
+        LifeLogNavHost(appState, Modifier.padding(it))
     }
 }
