657732db562a111a1b09f7562f147e649ece2bf3
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-12-28 14:20:43 +0900

    AndroidLogRule 추가
    - 테스트 환경에서 `android.util.Log`를 모의(mock) 처리합니다.
    - `Log.v`, `Log.d`, `Log.i`, `Log.w`, `Log.e`, `Log.wtf` 등 모든 로깅 호출이 0을 반환하도록 설정합니다.

---
+++ core/testing/src/main/java/com/bossmg/android/testing/rule/AndroidLogRule.kt
@@ -0,0 +1,31 @@
+package com.bossmg.android.testing.rule
+
+import android.util.Log
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.unmockkStatic
+import org.junit.rules.ExternalResource
+
+class AndroidLogRule: ExternalResource() {
+    override fun before() {
+        mockkStatic(Log::class)
+
+        every { Log.v(any(), any()) } returns 0
+        every { Log.v(any(), any(), any()) } returns 0
+        every { Log.d(any(), any()) } returns 0
+        every { Log.d(any(), any(), any()) } returns 0
+        every { Log.i(any(), any()) } returns 0
+        every { Log.i(any(), any(), any()) } returns 0
+        every { Log.w(any(), any<String>()) } returns 0
+        every { Log.w(any(), any<Throwable>()) } returns 0
+        every { Log.w(any(), any(), any()) } returns 0
+        every { Log.e(any(), any()) } returns 0
+        every { Log.e(any(), any(), any()) } returns 0
+        every { Log.wtf(any(), any<String>()) } returns 0
+        every { Log.wtf(any(), any<Throwable>()) } returns 0
+    }
+
+    override fun after() {
+        unmockkStatic(Log::class)
+    }
+}
